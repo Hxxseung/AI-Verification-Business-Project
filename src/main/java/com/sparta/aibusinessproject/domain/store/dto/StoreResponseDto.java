a942248dad7d950f120e7f3f72657276c502cb6f
Hxxseung <soaorsith2@naver.com>
2024-11-07 21:05:43 +0900

    feat: DTO 추가

---
+++ src/main/java/com/sparta/aibusinessproject/domain/store/dto/StoreResponseDto.java
@@ -0,0 +1,37 @@
+package com.sparta.aibusinessproject.domain.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StoreResponseDto {
+    private UUID storeId;
+
+    private UUID userId;
+
+    private String name;
+
+    private String category;
+
+    private String address;
+
+    private String description;
+
+    private String phone;
+
+    private String status;
+
+    private Timestamp createdAt;
+
+    private String createdBy;
+
+    private Timestamp modifiedAt;
+
+    private String modifiedBy;
+}
