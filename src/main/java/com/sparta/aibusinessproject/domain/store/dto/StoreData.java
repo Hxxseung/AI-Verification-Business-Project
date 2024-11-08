11de3e6e1e8097111f2422da68685fbb0f9aac26
Hxxseung <soaorsith2@naver.com>
2024-11-08 11:46:49 +0900

    refactor: Class -> record 변환

---
+++ src/main/java/com/sparta/aibusinessproject/domain/store/dto/StoreData.java
@@ -0,0 +1,48 @@
+package com.sparta.aibusinessproject.domain.store.dto;
+
+import com.sparta.aibusinessproject.domain.store.entity.Store;
+import lombok.Builder;
+
+@Builder
+public record StoreData(
+        String name,
+        String address,
+        String description,
+        String phone,
+        String status,
+        String createdBy,
+        String modifiedBy,
+        String deletedBy
+
+
+) {
+
+
+    // dto -> entity
+    public static Store toEntity(StoreData data) {
+        return Store.builder()
+                .name(data.name)
+                .address(data.address)
+                .description(data.description)
+                .phone(data.phone)
+                .status(data.status)
+                .createdBy(data.createdBy)
+                .modifiedBy(data.modifiedBy)
+                .deletedBy(data.deletedBy)
+                .build();
+    }
+
+    // entity -> dto
+    public static StoreData from(Store store) {
+        return StoreData.builder()
+                .name(store.getName())
+                .address(store.getAddress())
+                .description(store.getDescription())
+                .phone(store.getPhone())
+                .status(store.getStatus())
+                .createdBy(store.getCreatedBy())
+                .modifiedBy(store.getModifiedBy())
+                .modifiedBy(store.getDeletedBy())
+                .build();
+    }
+    }
