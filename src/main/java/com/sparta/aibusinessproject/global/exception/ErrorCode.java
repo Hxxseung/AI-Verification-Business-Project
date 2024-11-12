9a0a8505e4db370bacd877d740e1d8201830c0b8
HanBeom98 <Carry9691@gmail.com>
2024-11-12 12:17:15 +0900

    add: OrdernotFound

--- src/main/java/com/sparta/aibusinessproject/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/aibusinessproject/global/exception/ErrorCode.java
@@ -9,10 +9,13 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     //여기에 ErrorMessage 넣어서 쓰면 됩니다
     //ex) INVALID_STORE(HttpStatus.NOT_FOUND, "가게 정보가 없습니다.")
+    ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "Order not found"),
+
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "Product not found"),
     STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "Store not found"),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "Invalid request"),
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "Access denied");
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "Access denied"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
 
     private final HttpStatus status;
     private final String message;
