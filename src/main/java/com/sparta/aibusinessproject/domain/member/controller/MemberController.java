808181b746cb7e459eaf64ae0b0aa9c81f24c7d7
zoohee <jjhh1545@gmail.com>
2024-11-13 12:54:40 +0900

    feat: 유저 정보 조회 API 구현

--- src/main/java/com/sparta/aibusinessproject/domain/member/controller/MemberController.java
+++ src/main/java/com/sparta/aibusinessproject/domain/member/controller/MemberController.java
@@ -1,11 +1,19 @@
 package com.sparta.aibusinessproject.domain.member.controller;
 
+import com.sparta.aibusinessproject.domain.member.dto.response.MemberInfoResponse;
+import com.sparta.aibusinessproject.domain.member.entity.Member;
 import com.sparta.aibusinessproject.domain.member.service.MemberService;
+import com.sparta.aibusinessproject.global.exception.Response;
 import jakarta.validation.Valid;
 import com.sparta.aibusinessproject.domain.member.dto.request.SignupRequest;
 import com.sparta.aibusinessproject.domain.member.dto.response.LoginResponse;
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,5 +23,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public Response<MemberInfoResponse> getMemberInfo(@AuthenticationPrincipal UserDetails userDetails) {
+        return Response.success(memberService.getMemberInfo(userDetails.getUsername()));
+    }
 
 }
