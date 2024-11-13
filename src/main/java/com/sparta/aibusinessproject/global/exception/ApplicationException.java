b1db57039c226013a7cebecb50fa42bed505927a
Hxxseung <soaorsith2@naver.com>
2024-11-13 09:42:57 +0900

    add : Exception 관련 파일 추가(임시)

---
+++ src/main/java/com/sparta/aibusinessproject/global/exception/ApplicationException.java
@@ -0,0 +1,23 @@
+package com.sparta.aibusinessproject.global.exception;
+
+import com.sparta.aibusinessproject.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class ApplicationException extends RuntimeException {
+    private final ErrorCode errorCode;
+    private final String message;
+
+    public ApplicationException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+    }
+
+    @Override
+    public String toString() {
+        return "UserException{" +
+                "errorCode=" + errorCode +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/sparta/aibusinessproject/global/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.sparta.aibusinessproject.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+    DUPLICATED_STORENAME(HttpStatus.CONFLICT, "가게 이름이 중복됩니다."),
+    INVALID_STORE(HttpStatus.NOT_FOUND, "가게 정보가 없습니다."),
+    INVALID_CATEGORY(HttpStatus.NOT_FOUND, "카테고리 정보가 없습니다. 다른 카테고리를 입력해 주세요"),
+    DUPLICATED_CATEGORY(HttpStatus.CONFLICT, "카테고리 이름이 중복됩니다."),
+    NOTFOUND_CATEGORY(HttpStatus.NOT_FOUND, "카테고리가 존재하지 않습니다."),
+    DEFAULT_VALUE(HttpStatus.BAD_REQUEST, "기본 카테고리 종류 이므로 변경이 불가능합니다."),
+    ALREADY_CATEGORY(HttpStatus.ALREADY_REPORTED, "이미 존재하는 카테고리 입니다");
+
+    private final HttpStatus status;
+    private final String message;
+}
+
