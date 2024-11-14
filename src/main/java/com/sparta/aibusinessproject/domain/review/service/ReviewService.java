3f0885b5a75eeca0fc8d14fec549f6f809c1f29d
zoohee <jjhh1545@gmail.com>
2024-11-14 17:03:16 +0900

    feat: review 생성 기능 구현

--- src/main/java/com/sparta/aibusinessproject/domain/review/service/ReviewService.java
+++ src/main/java/com/sparta/aibusinessproject/domain/review/service/ReviewService.java
@@ -1,6 +1,11 @@
 package com.sparta.aibusinessproject.domain.review.service;
 
+import com.sparta.aibusinessproject.domain.order.entity.Orders;
+import com.sparta.aibusinessproject.domain.order.repository.OrdersRepository;
+import com.sparta.aibusinessproject.domain.review.dto.request.CreateReviewRequest;
+import com.sparta.aibusinessproject.domain.review.entity.Review;
 import com.sparta.aibusinessproject.domain.review.repository.ReviewRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +13,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+    private final OrdersRepository ordersRepository;
+    public UUID createReview(CreateReviewRequest request) {
+        Orders order = ordersRepository.getById(request.orderId());
+        Review review = reviewRepository.save(Review.create(request, order));
+        return review.getId();
+    }
 }
