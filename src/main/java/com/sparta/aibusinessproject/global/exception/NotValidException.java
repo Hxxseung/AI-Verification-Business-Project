00c663b2d2b5c4d9f129e57578cc90f49efe6901
Hxxseung <soaorsith2@naver.com>
2024-11-14 14:08:55 +0900

    remove : NotValidException.java 삭제

--- src/main/java/com/sparta/aibusinessproject/global/exception/NotValidException.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.aibusinessproject.global.exception;
-
-public class NotValidException extends ApplicationException {
-
-    public NotValidException(String message) {
-        super(ErrorCode.INVALID_REQUEST, message);
-    }
-}
